76fc23616bb3129e27c8b2f0b093a945c43ad0d6
Jason Ernst <ernstjason1@gmail.com>
2024-10-24 13:44:30 -0700

    setup gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+rootProject.name = "firebase-publishing-quarkus"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include("androidapp")
+include("quarkusapp")
