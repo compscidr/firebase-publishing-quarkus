aaf4037e65699b4b5b5dd64841e5ca1dd92121a3
Jason Ernst <ernstjason1@gmail.com>
2024-10-24 13:44:54 -0700

    Added android module

---
+++ androidapp/build.gradle.kts
@@ -0,0 +1,38 @@
+import com.google.firebase.appdistribution.gradle.firebaseAppDistribution
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.firebase.app.distribution)
+}
+
+android {
+    namespace = "com.jasonernst.kts"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.jasonernst.kts"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        multiDexEnabled = true
+    }
+
+    dependencies {
+        implementation(libs.androidx.appcompat)
+    }
+
+    buildTypes {
+        getByName("release") {
+            firebaseAppDistribution {
+                artifactType = "APK"
+                testers = "ernstjason1@gmail.com"
+            }
+        }
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
